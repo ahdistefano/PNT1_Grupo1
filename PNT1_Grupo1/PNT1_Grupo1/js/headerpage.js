6964d72ae13a3e654cf4ac8776fbf75750c2e979
Andres <ahdistefano@gmail.com>
2021-04-12 21:57:47 -0300

    Agregando headerpage.js

---
+++ PNT1_Grupo1/PNT1_Grupo1/js/headerpage.js
@@ -0,0 +1,14 @@
+﻿document.write(`
+    <!-- Barra superior con promos -->
+    <div id="barrasuperior"> <b>3, 6 y 12 cuotas sin interés</b> en productos de fabricación nacional en toda la WEB y en locales adheridos</div>
+
+    <!--Título principal-->
+    <h1 id="topHeader">Montanita</h1>
+
+    <!--Barra de navegación con links: -->
+    <nav id="barraNavegacion" class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="nav-link nav-fill menuEnlace" href="index.html">Inicio</a>
+        <a class="nav-link nav-fill menuEnlace" href="empresa.html">Empresa</a>
+        <a class="nav-link nav-fill menuEnlace" href="contacto.html">Contacto</a>
+    </nav>
+`);
